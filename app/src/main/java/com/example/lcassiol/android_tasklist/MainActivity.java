f1af51ccdb066d16cef1cbb349c6c566919beaf1
Cassio Carvalho <lccassiol@gmail.com>
2017-10-05 21:54:10 -0300

    Implement delete task after long click list item

--- app/src/main/java/com/example/lcassiol/android_tasklist/MainActivity.java
+++ app/src/main/java/com/example/lcassiol/android_tasklist/MainActivity.java
@@ -1,21 +1,26 @@
 package com.example.lcassiol.android_tasklist;
 
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
     private Button btnCreate;
 
     private SQLiteDatabase storage;
+
+    private ArrayAdapter<String> itemAdapter;
+    private ArrayList<Integer> ids;
+    private ArrayList<String> itens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +51,16 @@ public class MainActivity extends AppCompatActivity {
         btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 addTask(txtTask.getText().toString());
+            }
+        });
 
+        taskList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                alertConfirmAction(position);
+                return false;
             }
         });
     }
@@ -54,16 +70,21 @@ public class MainActivity extends AppCompatActivity {
             storage = openOrCreateDatabase("TakeANote", MODE_PRIVATE, null);
             storage.execSQL("CREATE TABLE IF NOT EXISTS Task(id INTEGER PRIMARY KEY AUTOINCREMENT,taskItem varchar)");
 
-            //String newTask = txtTask.getText().toString();
-            //storage.execSQL("INSERT INTO Task(taskItem) VALUES ('" + newTask + "')");
-
-            Cursor cursor = storage.rawQuery("SELECT * FROM Task", null);
+            Cursor cursor = storage.rawQuery("SELECT * FROM Task ORDER BY id DESC", null);
             int indexColumnId = cursor.getColumnIndex("id");
             int indexColumnTaskItem = cursor.getColumnIndex("taskItem");
+
+            itens = new ArrayList<>();
+            ids = new ArrayList<>();
+
+            itemAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_2, android.R.id.text1, itens);
+            taskList.setAdapter(itemAdapter);
 
             cursor.moveToFirst();
             while(cursor!=null){
                 Log.i("Logx", "ID: " + cursor.getString(indexColumnId) + " Task: " +  cursor.getString(indexColumnTaskItem));
+                ids.add(Integer.parseInt(cursor.getString(indexColumnId)));
+                itens.add(cursor.getString(indexColumnTaskItem));
                 cursor.moveToNext();
             }
         }catch(Exception e){
@@ -88,8 +109,29 @@ public class MainActivity extends AppCompatActivity {
             }
     }
 
-    private void Deletetask(){
+    private void deletetask(Integer id){
+            try {
+                storage.execSQL("DELETE FROM Task where id = " + id);
+                Toast.makeText(MainActivity.this, "Tarefa removida!", Toast.LENGTH_SHORT).show();
+                loadtasks();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+    }
 
+    private void alertConfirmAction(Integer id){
+        String selectTask = itens.get(id);
+        final Integer numberId = id;
+
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Aviso!")
+                .setMessage("Deseja remover '" + selectTask + "' ?")
+                .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        deletetask(ids.get(numberId));
+                    }
+                }).setNegativeButton("Não", null).show();
     }
 
 }
